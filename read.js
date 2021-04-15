91f5926cbe8b9168d30a202dbbe29165f04b9baf
Ray Lin <raylin009@gmail.com>
2021-04-14 20:17:13 -0700

    emailFormat logging proper data

--- read.js
+++ read.js
@@ -5,15 +5,25 @@ const emailRead = async (img_path) => {
   const text = await getText(img_path)
   const read = storeSalesParser(text)
   const emailFormat = (textObj) => {
+    console.table(textObj)
     return {
-      sales : textObj['Net sales'],
-      traffic : textObj['Traffic count'],
+      sales : textObj['Net sales'][1],
+      traffic : textObj['Traffic count'][0],
+      transaction : textObj['Transaction count'][0],
+      conversion : textObj['Conversion rate'][0]*100,
+      at: textObj['Net AT'][1],
+      upt: textObj['Net UPT'][0],
     }
   }
   const res = emailFormat(read)
+  return res
 }
 
 //Test emailRead with StoreSales04082021
+const img_path = `${__dirname}/photos/StoreSales04082021.jpg`
+emailRead(img_path)
+  .then(console.table)
+  .catch(console.log)
 
 const hourRead = async() => {
 
